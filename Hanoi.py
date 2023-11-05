8846d7abddb90f2a6439e6c422a79fdcb1232284
mohreha <mohreha77@gmail.com>
2023-11-05 22:14:31 +0330

    delete test case in hanoi

--- Hanoi.py
+++ Hanoi.py
@@ -1,4 +1,3 @@
-i = int(input("number of disks: "))
 def hanoi(disk=1,start='a',temp='b',end='c'):
     if disk==1:
         print(start + " go to " + end)
@@ -6,4 +5,3 @@ def hanoi(disk=1,start='a',temp='b',end='c'):
         hanoi(disk-1,start,end,temp)
         print(start + " go to " + end)
         hanoi(disk-1,temp,start,end)
-hanoi(i,'a','b','c') 
