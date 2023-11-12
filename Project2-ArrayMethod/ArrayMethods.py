cfef99c958e3af81998fff6a956f60245a4fefd9
shamimmeschian <meschianpanah@gmail.com>
2023-11-12 22:10:42 +0330

    delete method added

--- Project2-ArrayMethod/ArrayMethods.py
+++ Project2-ArrayMethod/ArrayMethods.py
@@ -6,3 +6,17 @@ def findfunc(inp, arr):
             c += 1
     if c == 0:
         print("not found")
+
+def delete_element(arr, n, x):
+    i = 0
+    while i < n:
+        if arr[i] == x:
+            break
+        i += 1
+
+    if i < n:
+        n -= 1
+        for j in range(i, n):
+            arr[j] = arr[j+1]
+    
+    return n
