20c0ab40a5bf99623cda4f30e2e2d6ac15f93053
mohreha <mohreha77@gmail.com>
2023-11-12 22:26:11 +0330

    insert method added

--- Project2-ArrayMethod/ArrayMethods.py
+++ Project2-ArrayMethod/ArrayMethods.py
@@ -14,9 +14,14 @@ def delete_element(arr, n, x):
             break
         i += 1
 
-    if i < n:
-        n -= 1
-        for j in range(i, n):
-            arr[j] = arr[j+1]
-    
-    return n
+def insertX(n, arr, x, pos):
+
+    newarr = [0] * (n + 1)
+    for i in range(n + 1):
+        if i < pos - 1:
+            newarr[i] = arr[i]
+        elif i == pos - 1:
+            newarr[i] = x
+        else:
+            newarr[i] = arr[i - 1]
+    return newarr
